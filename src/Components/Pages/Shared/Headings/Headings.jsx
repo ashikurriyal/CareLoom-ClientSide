beb3c1a1af5a031de92823272298e26624bd1645
ashikurriyal <riyal.ashikur@gmail.com>
2024-09-07 13:54:30 +0600

    Updated the headings for all sections

---
+++ src/Components/Pages/Shared/Headings/Headings.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+
+
+const Headings = ({ mainText, subText }) => {
+    return (
+        <div className='space-y-4'>
+            <div>
+                {
+                    mainText && <h1 className='text-center text-4xl font-bold text-primary-cyan mt-8 md:mt-32'>{mainText}</h1>
+                }
+            </div>
+            <div>
+                {
+                    subText && <h3 className='text-center'>{subText}</h3>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Headings;
