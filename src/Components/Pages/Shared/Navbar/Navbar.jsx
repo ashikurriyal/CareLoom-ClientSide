f6f734dcd7033ccd831fc3da931986c53ffda4f0
ashikurriyal <riyal.ashikur@gmail.com>
2024-09-21 21:23:04 +0600

    Updated the navbar slider

--- src/Components/Pages/Shared/Navbar/Navbar.jsx
+++ src/Components/Pages/Shared/Navbar/Navbar.jsx
@@ -12,7 +12,6 @@ const Navbar = () => {
       .catch((error) => console.error(error.message));
   };
 
-  //   const location = useLocation();
 
   //Dark Mode
   const { theme, setTheme } = localStorage.getItem("theme") || "light";
@@ -40,6 +39,7 @@ const Navbar = () => {
     { title: "Help", path: "" },
     { title: "Blogs", path: "" },
   ];
+
   return (
     <div className="navbar">
       <div className="navbar-start">
@@ -98,67 +98,6 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-end gap-4">
-        {/* {user ? (
-          <>
-            {user?.photoURL ? (
-              <div
-                className="tooltip tooltip-bottom avatar dropdown dropdown-end z-10"
-                data-tip={user.displayName}
-              >
-                <div
-                  tabIndex={0}
-                  role="button"
-                  className="lg:w-12 w-8 rounded-full ring ring-warning ring-offset-base-100 ring-offset-2"
-                >
-                  <img src={user.photoURL} alt={user.displayName} />
-                </div>
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-lg w-52"
-                >
-                  <li>
-                    <a onClick={handleLogOut} className="font-semibold text-sm">
-                      Sign Out
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <div
-                className="tooltip tooltip-bottom avatar dropdown dropdown-end z-10"
-                data-tip={user?.name}
-              >
-                <div
-                  tabIndex={0}
-                  role="button"
-                  className="lg:w-12 w-10 rounded-full ring ring-offset-base-100 ring-pink-600"
-                >
-                  <img
-                    src="https://i.ibb.co/rb1pJ40/ben-sweet-2-Lowvi-VHZ-E-unsplash.jpg"
-                    alt="Default Avatar"
-                  />
-                </div>
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-lg w-52"
-                >
-                  <li>
-                    <a onClick={handleLogOut} className="font-semibold text-sm">
-                      Sign Out
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </>
-        ) : (
-          <Link to="/login">
-            <button className="font-semibold text-xl btn btn-outline border-2 border-black hover:text-white hover:bg-black">
-              Log In
-            </button>
-          </Link>
-        )} */}
-
         {/* New */}
         {user ? (
           <>
@@ -170,10 +109,10 @@ const Navbar = () => {
                 <div
                   tabIndex={0}
                   role="button"
-                  className="lg:w-12 w-8 rounded-full ring ring-warning ring-offset-base-100 ring-offset-2"
+                  className="lg:w-12 w-8 rounded-full ring  ring-offset-base-100 ring-blue-500 ring-offset-2"
                   onClick={() => setDrawerOpen(!drawerOpen)} // Toggle drawer on avatar click
                 >
-                  <img src={user.photoURL} alt={user.displayName} />
+                  <img src={user?.photoURL} alt={user.displayName} />
                 </div>
               </div>
             ) : (
@@ -188,7 +127,7 @@ const Navbar = () => {
                   onClick={() => setDrawerOpen(!drawerOpen)} // Toggle drawer on avatar click
                 >
                   <img
-                    src="https://i.ibb.co/rb1pJ40/ben-sweet-2-Lowvi-VHZ-E-unsplash.jpg"
+                    src="https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="
                     alt="Default Avatar"
                   />
                 </div>
@@ -206,7 +145,7 @@ const Navbar = () => {
         {/* Drawer component */}
         <div
           id="drawer-navigation"
-          className={`fixed top-0 right-0 z-40 w-64 h-screen p-4 overflow-y-auto transition-transform ${
+          className={`fixed top-0 right-0 z-40 w-64 h-screen p-4 overflow-y-auto transition-transform shadow-xl  ${
             drawerOpen ? "translate-x-0" : "translate-x-full"
           } bg-white dark:bg-gray-800`} // Opens from the right
           tabIndex="-1"
@@ -239,8 +178,25 @@ const Navbar = () => {
             </svg>
             <span className="sr-only">Close menu</span>
           </button>
-          <div className="py-4 overflow-y-auto">
+          <div className="py-4 ">
             <ul className="space-y-2 font-medium">
+              <li className="flex flex-col items-center mt-6 -mx-2 md:mb-6">
+                <img
+                  className="object-cover w-24 h-24 mx-2 rounded-full ring-4 ring-offset-base-100 ring-blue-500"
+                  src={
+                    user?.photoURL
+                      ? user.photoURL
+                      : "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="
+                  }
+                  alt="avatar"
+                />
+                <h4 className="mx-2 mt-2 font-medium text-blue-600 dark:text-gray-200">
+                  {user?.displayName}
+                </h4>
+                <p className="mx-2 mt-1 text-sm font-medium text-blue-600 dark:text-gray-400">
+                  {user?.email}
+                </p>
+              </li>
               <li>
                 <a
                   href="#"
@@ -257,6 +213,84 @@ const Navbar = () => {
                     <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                   </svg>
                   <span className="ml-3">Dashboard</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                >
+                  <svg
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <span className="ml-3">Profile</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                >
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
+                  </svg>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Inbox</span>
+                  <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
+                    3
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  href="#"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 23 23"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                  <span className="flex-1 ms-3 whitespace-nowrap">
+                    Settings
+                  </span>
                 </a>
               </li>
               <li>
